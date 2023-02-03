07ec1e99b78bc0ca8d95f13a7559bb6ebda7c299
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-02-03 11:29:42 +0800

    update function

--- 最大公约数和最小公倍数.py
+++ 最大公约数和最小公倍数.py
@@ -1,5 +1,6 @@
 def gcd(x, y):
     """求最大公约数"""
+    """取两个数中比较小的数进行计算，循环较小的数到1， 直到满足同时被整除"""
     (x, y) = (y, x) if x > y else (x, y)
     for factor in range(x, 0, -1):
         if x % factor == 0 and y % factor == 0:
@@ -8,6 +9,7 @@ def gcd(x, y):
 
 def lcm(x, y):
     """求最小公倍数"""
+    """两个数相乘除以最大公约数就是最小公倍数"""
     return x * y // gcd(x, y)
 
 
