6bd8ab2f73411d8dfa8c6faf7340daaf674e2843
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-02-13 14:26:38 +0800

    add gui function

---
+++ GUI/GUI_development.py
@@ -0,0 +1,43 @@
+import tkinter
+import tkinter.messagebox
+
+
+def main():
+    flag = True
+
+    # 修改标签上的文字
+    def change_label_text():
+        nonlocal flag
+        flag = not flag
+        color, msg = ('red', 'babe I love you!')\
+            if flag else ('blue', 'you are so stupid!')
+        label.config(text=msg, fg=color)
+
+    # 确认退出
+    def confirm_to_quit():
+        if tkinter.messagebox.askokcancel('温馨提示', 'Are you sure leave this page?'):
+            top.quit()
+
+    # 创建顶层窗口
+    top = tkinter.Tk()
+    # 设置窗口大小
+    top.geometry('240x160')
+    # 设置窗口标题
+    top.title('show my love')
+    # 创建标签对象并添加到顶层窗口
+    label = tkinter.Label(top, text='babe I love you!', font='Arial -32', fg='red')
+    label.pack(expand=1)
+    # 创建一个装按钮的容器
+    panel = tkinter.Frame(top)
+    # 创建按钮对象 指定添加到哪个容器中 通过command参数绑定事件回调函数
+    button1 = tkinter.Button(panel, text='change', command=change_label_text)
+    button1.pack(side='left')
+    button2 = tkinter.Button(panel, text='exit', command=confirm_to_quit)
+    button2.pack(side='right')
+    panel.pack(side='bottom')
+    # 开启主事件循环
+    tkinter.mainloop()
+
+
+if __name__ == '__main__':
+    main()
