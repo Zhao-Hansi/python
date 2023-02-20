51692361d09d24291cd1a888f7d2248c81e3021d
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-02-20 17:52:27 +0800

    add file operation

---
+++ file/file_open.py
@@ -0,0 +1,43 @@
+def main():
+    f = None
+    try:
+        f = open('致橡树.txt', 'r', encoding='utf-8')
+        print(f.read())
+    except FileNotFoundError:
+        print('无法打开指定的文件!')
+    except LookupError:
+        print('指定了未知的编码!')
+    except UnicodeDecodeError:
+        print('读取文件时解码错误!')
+    finally:
+        if f:
+            f.close()
+
+
+# if __name__ == '__main__':
+#     main()
+def main1():
+    f = None
+    try:
+        f = open('/Users/zhaozexin/PycharmProjects/python/100个数求和.py', encoding= 'utf-00')
+        print(f.read())
+    except FileNotFoundError:
+        print('cannot open file')
+    except LookupError:
+        print('itis a wrong encoding')
+    except UnicodeDecodeError:
+        print('read file has a wrong behavior')
+    finally:
+        if f:
+            f.close()
+
+def main2():
+    with open('/Users/zhaozexin/PycharmProjects/python/100个数求和.py', mode= 'r') as f:
+        for line in f:
+            print(line)
+    print('-------------------------')
+    with open('/Users/zhaozexin/PycharmProjects/python/100个数求和.py', mode= 'r') as f1:
+        print(f1.readline())
+
+if __name__ == '__main__':
+    main2()
---
+++ file/request.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+def main():
+    resp = requests.get('https://apis.tianapi.com/guonei/index?key=6348f8b66f9f95e386ef0d4157271e89&num=100')
+    print(type(resp))
+    data_model = json.loads(resp.text)
+    print(type(data_model))
+    print(data_model)
+    context = data_model['newslist']
+    for news in context['newslist']:
+        print(news)
+        print(news['title'])
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
