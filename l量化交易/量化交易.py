5e90a2ff5379c60151e7bb833ac6369bde95cfbe
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-03-14 22:56:56 +0800

    add files

---
+++ l量化交易/量化交易.py
@@ -0,0 +1,6 @@
+import json
+import requests
+gemini_ticker = 'https://api.gemini.com/v1/pubticker/{}'
+symbol = 'btcusd'
+btc_data = requests.get(gemini_ticker.format(symbol)).json()
+print(json.dumps(btc_data, indent=4))
