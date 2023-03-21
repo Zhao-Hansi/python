6e6416047c6d0ecd313239d6af575a629a5f9901
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-03-21 15:24:51 +0800

    decorate order of execution

--- 装饰器.py
+++ 装饰器.py
@@ -8,15 +8,25 @@ def repeat(num):
             for i in range(num):
                 print('wrapper of decorator')
                 func(*args, **kwargs)
+
         return wrapper
+
     return my_decorator
 
 
+def log_execution_time(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('装饰器1')
+        return func(*args, **kwargs)
+    return wrapper
+
+
 @repeat(4)
+@log_execution_time
 def greet(message):
     print(message)
 
 
 if __name__ == '__main__':
-    print(greet.__name__)
     greet('hello world')
