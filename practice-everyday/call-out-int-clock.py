03ba790acdc40e744cd41a66c9d7b7435810ccd4
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-06-15 16:02:22 +0800

    clock

--- practice-everyday/call-out-int-clock.py
+++ practice-everyday/call-out-int-clock.py
@@ -4,3 +4,33 @@
 
 功能描述： 运行后，在每一个整点长响一声，半个整点短响两声。实现睡眠模式，晚上十二点到早上六点不响铃。
 """
+import time
+
+while True:
+    current_time = time.localtime()
+    hour, minute = current_time.tm_hour, current_time.tm_min
+
+    if 6 <= hour < 12:
+        if minute == 0:
+            print("长响一声")
+            time.sleep(60)
+        elif minute == 30:
+            print("短响两声")
+            time.sleep(60)
+    elif 12 <= hour < 18:
+        if minute == 0:
+            print("长响一声")
+            time.sleep(60)
+        elif minute == 30:
+            print("短响两声")
+            time.sleep(60)
+    elif 18 <= hour < 24:
+        if minute == 0:
+            print("长响一声")
+            time.sleep(60)
+
+        elif minute == 30:
+            print("短响两声")
+            time.sleep(60)
+    else:
+        time.sleep(60 * 60 * 6)  # 晚上12点到早上6点不响铃，等待6个小时
