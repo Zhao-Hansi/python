92e1b4098b8877c56025c4edf62bc4d2f5957408
zexin.zhao <zexin.zhao@thoughtworks.com>
2023-10-30 17:17:45 +0800

    use decorate to caculate the function time

---
+++ python-code-practice/decorate.py
@@ -0,0 +1,21 @@
+import time
+
+
+def calculate_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"函数 {func.__name__} 的执行时间为: {execution_time} 秒")
+        return result
+
+    return wrapper
+
+
+@calculate_execution_time
+def my_function():
+    time.sleep(2)
+
+
+my_function()
